3137d87de22555ef45139e8477a98e1477d8f85d
Dilina Perera <dilinacperera@gmail.com>
2025-07-07 15:17:36 +0530

    Add function to load and print system prompt context
    Introduces get_system_prompt_context to read a PDF and summary text file, concatenate their contents, and print the resulting system prompt. This prepares context for further use, likely in a conversational or AI application.

--- app.py
+++ app.py
@@ -9,3 +9,22 @@ import gradio as gr
 
 load_dotenv(override = True)
 
+def get_system_prompt_context():
+    reader = PdfReader("./me/Profile.pdf")
+    context = "Linkedin Profile: \n \n"
+    for page in reader.pages:
+        text = page.extract_text()
+        if text:
+            context += text + "\n\n\n"
+
+    summary = open("./me/summary.txt", 'r').read()
+    
+    context += "Dilina Perera Summary: \n\n"
+
+    context += summary
+
+    return context
+
+system_prompt = get_system_prompt_context()
+
+print(system_prompt)
